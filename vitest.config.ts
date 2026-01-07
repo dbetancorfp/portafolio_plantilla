ed77c1b0d4f9d50f2d96c99be42b99f57146fab2
dbetancorfp <dbetancor.fp@gmail.com>
2026-01-07 11:08:22 +0000

    Modificacion de setting

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,6 +7,11 @@ export default mergeConfig(
   defineConfig({
     test: {
       environment: 'jsdom',
+      globals: true,
+      coverage: {
+        reporter: ['text', 'json', 'html'],
+        reportsDirectory: './coverage',
+      },
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
     },
